ed4e9da56b019684cf60c45c1d3add605d0084a3
93geraldtee <93geraldtee@gmail.com>
2020-03-04 13:15:10 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+
+  #Rails route: HTTP verb/request (get) accepts a hash, whose key is the URL ('/students') 
+  #whose value is the CONTROLLERclass#ACTIONmethod
   
+  # get '/students' to: 'students#index'   --> conventional syntax
+         # URL           class#method
+
+  get({'/students' => 'students#index'})
+
 end
